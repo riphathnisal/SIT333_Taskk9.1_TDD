70f2a5fcb62f59577955d17a5a0c96796b683dd5
riphathnisal <riphathnisal2002@gmail.com>
2025-05-22 01:30:48 +1000

    second commit

--- src/main/java/OnTrackTaskSubmit.java
+++ src/main/java/OnTrackTaskSubmit.java
@@ -1,4 +1,59 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OnTrackTaskSubmit {
+	
+	private List<Task> tasks = new ArrayList<>();
+	
+	public void taskSubmit(String taskId, String studentId, LocalDateTime dueDate) {
+		tasks.add(new Task(taskId, studentId, dueDate));
+	}
+	
+	public boolean markTaskSubmit(String taskId) {
+		Task task = taskFind(taskId);
+		if(task != null) {
+			task.SubmitSet(true);
+			return true;
+		}
+		return false;
+	}
+	
+	public Task taskFind(String taskId) {
+		for (Task task : tasks) {
+			if(task.getTaskId().equals(taskId)) {
+				return task;
+			}
+		}
+		return null;
+	}
+		
+	public String taskStatus(String taskId, LocalDateTime currentTime) {
+		Task task = taskFind(taskId);
+		if(task == null) {
+			return "Task not Found";
+		}
+		
+		boolean submitted = task.isSubmit();
+		LocalDateTime dueDate = task.getDuedate();
+		
+		if (submitted) return "Task Submitted!. You're On Track.";
+		
+		long timeLeft = Duration.between(currentTime, dueDate).toMinutes();
+		
+		if (timeLeft < 0) {
+			return "Deadline missed!. No Submission made.";
+		} else if (timeLeft < 60) {
+			return "Less than 1 hour left!. Make submission soon.";
+			
+		} else if (timeLeft < 1440) {
+			return "Less than 24 hours left!. Make submission soon.";
+			
+		}
+		
+		return "";
+	}
+	
 
 }
--- src/test/java/OnTrackTest.java
+++ src/test/java/OnTrackTest.java
@@ -18,11 +18,48 @@ public class OnTrackTest {
 	
 	@Test
 	public void testSubmitaTask() {
-		result.submitTask("SIT333P1", "s22312", now.plusDays(1));
-		Task task = result.findTaskbyID("SIT333P1");
+		result.taskSubmit("SIT333P1", "s22312", now.plusDays(1));
+		Task task = result.taskFind("SIT333P1");
 		assertNotNull(task);
 		assertEquals("s22312", task.getStudentId());
 	}
 
+	@Test
+	public void testStatuslessthanday() {
+		result.taskSubmit("SIT333P2", "s22312", now.plusHours(23));
+		String output = result.taskStatus("SIT333P2", now);
+		assertEquals("Less than 24 hours left!. Make submission soon.", output);
+	}
+	
+	@Test
+	public void testStatuslessanHour() {
+		result.taskSubmit("SIT333P3", "s22312", now.plusMinutes(55));
+		String output = result.taskStatus("SIT333P3", now);
+		assertEquals("Less than 1 hour left!. Make submission soon.", output);
+	}
+	
+	@Test
+	public void testStatusDeadlinepass() {
+		result.taskSubmit("SIT333P4", "s22312", now.minusMinutes(10));
+		String output = result.taskStatus("SIT333P4", now);
+		assertEquals("Deadline missed!. No Submission made.", output);
+	}
+	
+	@Test
+	public void testSubmittedTest() {
+		result.taskSubmit("SIT333P5", "s22312", now.plusHours(30));
+		result.markTaskSubmit("SIT333P5");
+		String output = result.taskStatus("SIT333P5", now);
+		assertEquals("Task Submitted!. You're On Track.", output);
+	
+	}
+	
+	@Test
+	public void taskNotFound() {
+		String output = result.taskStatus("NOTASK", now);
+		assertEquals("Task not Found", output);
+	}
+	
+	
 	
 }
